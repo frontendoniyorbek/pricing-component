5ee7667a0120383dc56b19f5154f4998e3089f29
frontendoniyorbek <tursunovdoniyorbek077@gmail.com>
2022-04-21 11:03:14 -0700

    Add js

--- js/main.js
+++ js/main.js
@@ -2,5 +2,12 @@ document.querySelector('input.our-pricing-input').addEventListener('click',()=>{
   const elSiteThemeTogglers = document.querySelectorAll('.about-item .about-value');
   elSiteThemeTogglers.forEach(item => {
     item.textContent = Number(item.textContent)+1
-  })
+  });
+});
+
+document.querySelector('input.our-pricing-input').addEventListener('click',()=>{
+  const elSiteThemeTogglers = document.querySelectorAll('.about-item .about-value');
+  elSiteThemeTogglers.forEach(item => {
+    item.textContent = Number(item.textContent)+1
+  });
 })
