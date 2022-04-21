490e2ed4e7fbf629d68595e1c226db9f512c6e62
frontendoniyorbek <tursunovdoniyorbek077@gmail.com>
2022-04-21 07:57:11 -0700

    Add html and jc and css

---
+++ js/main.js
@@ -0,0 +1,6 @@
+document.querySelector('input.our-pricing-input').addEventListener('click',()=>{
+  const elSiteThemeTogglers = document.querySelectorAll('.about-item .about-value');
+  elSiteThemeTogglers.forEach(item => {
+    item.textContent = Number(item.textContent)+1
+  })
+})
