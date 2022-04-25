d5e5d225ec96a131ff890fc8b5b7ca4355bca27b
frontendoniyorbek <tursunovdoniyorbek077@gmail.com>
2022-04-24 21:05:20 -0700

    Add js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
-document.querySelector('input.our-pricing-input').addEventListener('click',()=>{
-  const elSiteThemeTogglers = document.querySelectorAll('.about-item .about-value');
-  elSiteThemeTogglers.forEach(item => {
-    item.textContent = Number(item.textContent)+1
+const elsPaymentPeriodRadio = document.querySelectorAll('[name="payment_period"]');
+const elsPricingPlanValue = document.querySelectorAll('.pricing-plan-value');
+
+elsPaymentPeriodRadio.forEach(function (elPaymentPeriodRadio) {
+  elPaymentPeriodRadio.addEventListener('change', function () {
+    const period = elPaymentPeriodRadio.value;
+
+    if (period === 'annualy') {
+      elsPricingPlanValue.forEach(function (elPrice) {
+        elPrice.textContent = elPrice.closest('.pricing-plan').dataset.paymentAnnualy;
+      });
+    }
+    else {
+      elsPricingPlanValue.forEach(function (elPrice) {
+        elPrice.textContent = elPrice.closest('.pricing-plan').dataset.paymentMonthly;
+      });
+    }
   });
 });
-
-document.querySelector('input.our-pricing-input').addEventListener('click',()=>{
-  const elSiteThemeTogglers = document.querySelectorAll('.about-item .about-value');
-  elSiteThemeTogglers.forEach(item => {
-    item.textContent = Number(item.textContent)+1
-  });
-})
